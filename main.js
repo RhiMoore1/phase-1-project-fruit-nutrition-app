524e2faa1934bec3f04343988f8627778169948c
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-05-08 07:49:51 -0700

    creates dropdown list options in main.js

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ console.log("main.js is working");
 
 fetch("http://localhost:3000/Fruits")
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(fruits => {
+        // this is the select buttonsId
+        const fruitList = document.getElementById('fruitList');
+        fruits.forEach(fruit => {
+            // create an option for each individual fruit
+            const option = document.createElement('option');
+            // set the option's value and text to the fruit's name
+            option.value = fruit.name;
+            option.text = fruit.name;
+            // apend each fruit name to option value and text
+            fruitList.appendChild(option);
+        })
+    });
 
 
