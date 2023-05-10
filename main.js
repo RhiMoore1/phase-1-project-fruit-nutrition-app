2d61d3e0a456af60946134e0995be4f4e3e69a1a
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-05-10 12:11:12 -0700

    adds init function

--- main.js
+++ main.js
@@ -4,66 +4,17 @@ console.log("main.js is working");
 const MAX_CARDS = 3;
 let numCards = 0;
 
-fetch("http://localhost:3000/Fruits")
+
+
+function init() {
+    fetch("http://localhost:3000/Fruits")
     .then(res => res.json())
-    .then(fruits => {
-        // this is the select buttonsId
-        // const fruitList = document.getElementById('fruitList');
-        
-        // // create a placeholder option in select dropdown list
-        // const placeholderOption = document.createElement('option');
-        // // makes placeholder unselectable
-        // placeholderOption.disabled = true;
-        // // sets as default
-        // placeholderOption.selected = true;
-        // // sets text and append
-        // placeholderOption.text = "Fruit";
-        // fruitList.appendChild(placeholderOption);
-
-        // fruits.forEach(fruit => {
-        //     // create an option for each individual fruit
-        //     const option = document.createElement('option');
-        //     // set the option's value and text to the fruit's name
-        //     option.value = fruit.name;
-        //     option.text = fruit.name;
-        //     // apend each fruit name to option value and text
-        //     fruitList.appendChild(option);
-        // })
-
-        // add submit button event listener to the form
-        // const selectFruitForm = document.getElementById('selectFruitForm');
-        // selectFruitForm.addEventListener('submit', (e) => {
-        //     e.preventDefault();
-
-        //     // set max on fruit comparison selections
-        //     if(numCards >= MAX_CARDS) {
-        //         alert('You have selected the maximum number of fruit comparisons');
-        //         return;
-        //     }
-
-        //     // retrieve selected fruit from dropdown list
-        //     const fruitListValue = document.getElementById('fruitList').value;
-        //     // search through fruits object and find fruit name that matches the selected dropdown list fruit
-        //     // set fruit variable to matching dropdown selection
-        //     const fruit = fruits.find(fruit => fruit.name === fruitListValue);
-            
-        //     // call createCard and increment
-        //     createCard(fruit);
-        //     numCards ++;
-
-        //     // grab clear button and attach eventListener
-        //     const clearButton = document.getElementById('clearButton');
-        //     clearButton.addEventListener('click', clearSelections);
-
-        //     // set the selected index of fruitList to 0 so placeholder shows and clear divs
-        //     fruitList.selectedIndex = 0;
-
-        //     // call showFruitForm
-        //     showFruitForm();
-            
-        //});
+    .then(fruits => { 
+        createSelectionOptions(fruits);
+        handleSelectFruitFormSubmit(fruits);
     });
-
+}    
+init();
 
 
 function createSelectionOptions(fruits) {
